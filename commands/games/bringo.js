3cce312d679f3ed8a087f8c3f4bb022b101341ad
Will Sullivan <furtivespy@gmail.com>
2021-02-11 08:53:33 -0800

    that was dumb

--- commands/games/bringo.js
+++ commands/games/bringo.js
@@ -119,7 +119,7 @@ class Bringo extends Command {
                 } else if (args[0] && ( args[0].toLowerCase() === "score" || args[0].toLowerCase() === "scores")){
                     var sorted = Object.keys(BringoData.scoreboard).map(c => ({key: c, value: BringoData.scoreboard[c]})).sort((a,b) => b.value - a.value)
                     var scoreMessage = ""
-                    await message.guild.members.fetch();
+                    message.guild.members.fetch();
                     for(let i=0;i<sorted.length;i++){
                         var icon = (i===0) ? ":first_place:" : (i===1) ? ":second_place:" : (i===2) ? ":third_place:" : ":medal:"
                         
