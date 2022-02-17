accc8150ccb09e34769b5de3fe0d3c76718559ae
Will Sullivan <furtivespy@gmail.com>
2022-02-16 16:28:14 -0800

    inspire moved to /inspire

---
+++ slashcommands/fun/inspire.js
@@ -0,0 +1,40 @@
+const SlashCommand = require('../../base/SlashCommand.js')
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const fetch = require('node-fetch');
+const { MessageEmbed } = require('discord.js');
+
+class Inspire extends SlashCommand {
+    constructor(client){
+        super(client, {
+            name: "inspire",
+            description: "Get's an AI generated inspirational image & quote",
+            usage: "Get's an AI generated inspirational image & quote",
+            enabled: true,
+            permLevel: "User"
+          })
+        this.data = new SlashCommandBuilder()
+            .setName(this.help.name)
+            .setDescription(this.help.description)  
+    }
+
+    async execute(interaction) {
+        try {
+            await interaction.deferReply()
+
+            let res = await fetch('http://inspirobot.me/api?generate=true')
+            let text = await res.text()
+            
+            const inspiration = new MessageEmbed()
+                .setColor(6682360)
+                .setImage(text)
+                .setFooter({text: `Powered by inspirobot.me`})
+
+            interaction.editReply({embeds: [inspiration]})
+                
+        } catch (e) {
+            this.client.logger.log(e,'error')
+        }
+    }
+}
+
+module.exports = Inspire
